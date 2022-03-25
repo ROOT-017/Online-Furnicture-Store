efc4c337246251e5dcb8d829868029285e9bd5f1
NKWETACHA TERENCE NGWEN <nkwetachaterence17@gmail.com>
2022-03-25 14:17:05 +0100

    JS file added

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$("img#item_image").hover(function() {
+    var selectedColor = "#F2F2F2"
+    var textColor = "#ffff"
+    var blackColor = "#000"
+        // var text = $(".item-container").css("background-color", textColor)
+    $("#item-container").removeAttr(attr = "id", value = "item-container")
+        //$("#item-container").setAttr(attr = "id", value = "item-container")
+        /*  var container = $(".items_container")
+         for (i = 0; i < 3; i++) {
+             console.log(container[i])
+         }
+         /*   */
+        /*  */
+        /*     this.css("background-color", selectedColor)
+         */
+        /*  var thisObj = this
+         thisObj.css("background-color", selectedColor) */
+        /*  console.log(thisObj) */
+})
